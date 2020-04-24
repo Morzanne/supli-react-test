942f6ebcfc79551d98025cd1e52c421eeda00124
Aurélien <a.grenet.dev@gmail.com>
2020-04-24 17:08:28 +0200

    config/redux-store and main architectural folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import store from './store';
+import saga from './sagas/';
+
+import { sagaMiddleware } from './middleware';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    HELLO
+  </Provider>
+);
 
 export default App;
+
+sagaMiddleware.run(saga);
